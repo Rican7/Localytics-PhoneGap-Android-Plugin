d36f7683e2e5cf383a0400ca0f6653778a3868a2
Trevor Suarez <rican7@gmail.com>
2012-08-24 13:01:49 -0400

    Created a better test view to test screens/events. :)

--- assets/www/behavior.js
+++ assets/www/behavior.js
@@ -1,10 +1,9 @@
 window.deviceOS = 'android';
 
+var ghettoTestFunction = null; // So sad
+
 // Let's wait for PhoneGap to load and to fire the deviceready event
 document.addEventListener('deviceready', function() {
-	// Tag the screen
-	LocalyticsPlugin.tagScreen('Main Plugin Test View');
-
 	// Tag an event
 	var eventData = {
 		"deviceOS": deviceOS,
@@ -14,4 +13,34 @@ document.addEventListener('deviceready', function() {
 	console.log('Event Data:');
 	console.log(eventData);
 
+	// Let's cache a couple of our "selectors" (hahahaha)
+	var mainPage = document.getElementById('main-page');
+	var aboutPage = document.getElementById('about-page');
+
+	ghettoTestFunction = function(linkBool) {
+		if (linkBool) {
+			mainPage.setAttribute("style", "display: block"); // God, this is gross
+			aboutPage.setAttribute("style", "display: none");
+
+			// Tag the screen
+			LocalyticsPlugin.tagScreen('Main Plugin Test View');
+		}
+		else {
+			mainPage.setAttribute("style", "display: none");
+			aboutPage.setAttribute("style", "display: block");
+
+			// Tag the screen
+			LocalyticsPlugin.tagScreen('About View');
+		}
+
+		// Tag the event!
+		LocalyticsPlugin.tagEvent('View Switched', {
+			"The view was what?!": "Switched!",
+			"LinkBool": linkBool,
+			"Wrote By": "Trevor N. Suarez (Rican7)"
+		});
+	}
+
+	// Initialize, bitch
+	ghettoTestFunction(true);
 }, false);
